a1788d04cff5db6db989ab31e6a2dd2011a2f531
Andrew <ghobrial.andrew@gmail.com>
2019-03-23 15:52:12 -0400

    navbar v1

--- frontend/client/src/components/NavBar.js
+++ frontend/client/src/components/NavBar.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Navigation = styled.section`
+  height: 4rem;
+  width: 100%;
+  background: #6200EE;
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+`;
 
 class NavBar extends Component {
   render() {
     return (
-      <div className="news-card">
-        <div>Medius News</div>
-        <div>Why I built This</div>
+      <Navigation>
+        <h2>Medius News</h2>
+        <p>Why I built This</p>
         <br></br>
-      </div>
+      </Navigation>
     );
   }
 }

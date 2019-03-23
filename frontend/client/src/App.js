62bb9d0e32ac7496ae01b7b8b37c80673703dcb2
Andrew <ghobrial.andrew@gmail.com>
2019-03-22 23:50:58 -0400

    added material-ui css framework

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import NewsGrid from './components/NewsGrid'
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>navbar</div>
+        {/* <NavBar/> */}
         <NewsGrid/>
       </div>
     );
